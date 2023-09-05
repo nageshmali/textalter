2064173921bd89aae65759482c94530cb1fe1613
Nagesh Mali <malinagesh60@gmail.com>
2023-09-05 18:48:09 +0530

    removed home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,6 @@ export default function Navbar(props) {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Home 
-              </a>
-            </li>
           </ul>
 
           <div className={`custom-control custom-switch mr-5 text-${props.theme==='light'?'dark':'light'}`}>
